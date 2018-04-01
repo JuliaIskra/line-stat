c3de72d2ebbef149e5d8f6e3b0930726256ba4ff
Julia Nemtseva <juliaiskra@gmail.com>
2018-04-01 18:56:33 +0300

    Add mode to find long lines

--- src/line_stat/core.clj
+++ src/line_stat/core.clj
@@ -1,13 +1,38 @@
 (ns line-stat.core
   (:gen-class))
 
+(defn filter-big-lines
+  [line max-size]
+  (if (> (.length line) max-size)
+    line
+    nil))
+
 (defn -main
-  [dir & args]
-  (->> (file-seq (java.io.File. dir))
-       (filter (fn [f] (.endsWith (.getPath f) ".java")))
-       (map clojure.java.io/reader)
-       (map line-seq)
-       flatten
-       (remove nil?)
-       (map (fn [line] (.length line)))
-       frequencies))
+  [mode & args]
+  (cond
+    (= mode "stat")
+    (let [dir (first args)]
+      (->> (file-seq (java.io.File. dir))
+           (filter (fn [f] (.endsWith (.getPath f) ".java")))
+           (remove (fn [f] (.contains (.getPath f) "/test/")))
+           (map clojure.java.io/reader)
+           (map line-seq)
+           flatten
+           (remove nil?)
+           (map (fn [line] (.length line)))
+           frequencies
+           prn))
+
+    (= mode "lines")
+    (let [dir      (first args)
+          max-size (second args)]
+      (->> (file-seq (java.io.File. dir))
+           (filter (fn [f] (.endsWith (.getPath f) ".java")))
+           (remove (fn [f] (.contains (.getPath f) "/test/")))
+           (map clojure.java.io/reader)
+           (map line-seq)
+           flatten
+           (remove nil?)
+           (map #(filter-big-lines % (Integer. max-size)))
+           (remove nil?)
+           prn))))
