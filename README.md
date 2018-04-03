58965e9c5f70ed15adb6a9ac00b6b4f1763a34e9
Julia Nemtseva <juliaiskra@gmail.com>
2018-04-03 11:18:31 +0300

    Add license, readme and help output

---
+++ README.md
@@ -0,0 +1,40 @@
+##Lines size analyzer in Java code
+
+###How to build
+`lein uberjar`
+
+###How to use
+To get stat on lines sizes:
+
+`java -jar target\uberjar\line-stat-*-standalone.jar stat path/to/java/project`
+
+Sample output:
+```
+lines   count
+ 
+ 0      54
+ 1      22
+ 2      1
+ 4      106
+ 5      114
+ 8      4
+ 9      65
+ 10     34
+ ```
+ 
+ To get lines longer than specified size:
+ 
+ `java -jar target\uberjar\line-stat-*-standalone.jar lines path/to/java/project 78`
+ 
+ Sample output:
+ ```
+ 
+ ================================================================================
+ ..\..\Java\calculator\expression\Division.java:
+         double denominator = Math.pow(op2.real, 2) + Math.pow(op2.imaginary, 2);
+ 
+ ================================================================================
+ ..\..\Java\calculator\expression\Subtraction.java:
+     public Subtraction(int nesting, Expression operand1, Expression operand2) {
+ ```
+ 
--- src/line_stat/core.clj
+++ src/line_stat/core.clj
@@ -1,6 +1,22 @@
 (ns line-stat.core
   (:gen-class))
 
+(defn print-line-stat
+  [dir]
+  (println (str "lines\tcount\n"))
+  (->> (file-seq (java.io.File. dir))
+       (filter (fn [f] (.endsWith (.getPath f) ".java")))
+       (map clojure.java.io/reader)
+       (map line-seq)
+       flatten
+       (remove nil?)  ; for empty files
+       (map (fn [line] (.length line)))
+       frequencies
+       (sort-by first)
+       (map (fn [[l c]] (str l "\t\t" c "\n")))
+       (reduce str)
+       (println)))
+
 (defn filter-big-lines
   [line max-size]
   (if (> (.length line) max-size)
@@ -14,41 +30,39 @@
        (reduce str (interpose "\n" big-lines))
        "\n\n"))
 
-(defn -main
-  [mode & args]
-  (cond
-    (= mode "stat")
-    (let [dir (first args)]
-      (println (str "lines\tcount\n"))
-      (->> (file-seq (java.io.File. dir))
-           (filter (fn [f] (.endsWith (.getPath f) ".java")))
-           ;(remove (fn [f] (.contains (.getPath f) "/test/")))
-           (map clojure.java.io/reader)
-           (map line-seq)
-           flatten
-           (remove nil?) ; for empty files
-           (map (fn [line] (.length line)))
-           frequencies
-           (sort-by first)
-           (map (fn [[l c]] (str l "\t\t" c "\n")))
-           (reduce str)
-           (println)))
+(defn print-big-lines
+  [dir max-size]
+  (let [files     (->> (file-seq (java.io.File. dir))
+                       (filter (fn [f] (.endsWith (.getPath f) ".java"))))
+        big-lines (for [file files
+                        :let [lines (->> file
+                                         clojure.java.io/reader
+                                         line-seq
+                                         (map #(filter-big-lines % (Integer. max-size)))
+                                         (remove nil?))]
+                        :when (not (empty? lines))]
+                    [(.getPath file) lines])]
+    (->> big-lines
+         (map print-lines)
+         (reduce str)
+         println)))
 
-    (= mode "lines")
-    (let [dir       (first args)
-          max-size  (second args)
-          files     (->> (file-seq (java.io.File. dir))
-                         (filter (fn [f] (.endsWith (.getPath f) ".java"))))
-          ;(remove (fn [f] (.contains (.getPath f) "/test/")))
-          big-lines (for [file files
-                          :let [lines (->> file
-                                           clojure.java.io/reader
-                                           line-seq
-                                           (map #(filter-big-lines % (Integer. max-size)))
-                                           (remove nil?))]
-                          :when (not (empty? lines))]
-                      [(.getPath file) lines])]
-      (->> big-lines
-           (map print-lines)
-           (reduce str)
-           println))))
+(defn -main
+  [& args]
+  (let [mode (first args)]
+    (cond
+      (= mode "stat")
+      (let [dir (second args)]
+        (print-line-stat dir))
+
+      (= mode "lines")
+      (let [dir      (second args)
+            max-size (nth args 2)]
+        (print-big-lines dir max-size))
+
+      :else
+      (do
+        (println "Unexpected input")
+        (println "This program accepts two types of input:")
+        (println "stat /path/to/java/project # to show stat on lines")
+        (println "lines /path/to/java/project $line-size # to show lines longer than $line-size")))))
